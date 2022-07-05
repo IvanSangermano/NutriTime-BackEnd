d59f69738058bf210abe3597a8626e936d0cdf55
Matias Virgili <matiasvirgili@gmail.com>
2022-07-05 11:20:04 -0300

    Controller exercises

--- src/Controller/exercise-controller.js
+++ src/Controller/exercise-controller.js
@@ -43,3 +43,68 @@ const getExercise = async (req = request, res = response) => {
     res.status(500).json({ error: 'An error has occurred' });
   }
 };
+
+const postExcercise = async (req = request, res = response) => {
+  try {
+    const exercise = new Exercise(req.body);
+    const exerciseExist = await Exercise.findOne(exercise);
+
+    if (exerciseExist) {
+      res.status(400).json({
+        error: 'Error, existing exercise',
+      });
+    } else {
+      await exercise.save();
+      res
+        .status(201)
+        .json({ message: 'Exercise added successfully', data: exercise });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'An error has ocurred' });
+  }
+};
+
+const putExercise = async (req = request, res = response) => {
+  try {
+    const exerciseId = req.params.id;
+    let exercise = req.body;
+
+    const exerciseExist = await Exercise.findOne(exerciseId);
+
+    if (exerciseExist) {
+      return res.status(400).json({ error: 'Error, existing Exercise' });
+    } else {
+      exercise = await Exercise.findByIdAndUpdate(exerciseId, exercise, {
+        new: true,
+      });
+    }
+    if (exercise) {
+      res.json({ data: exercise });
+    } else {
+      res.status(404).json({ error: 'Exercise doesn´t exist' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'An error has occurred' });
+  }
+};
+
+const deleteExercise = async (req = request, res = response) => {
+  try {
+    const exerciseId = req.params.id;
+    const exercise = await Exercise.findByIdAndDelete(exerciseId);
+
+    if (exercise) {
+      res.json({ message: 'Exercise deleted successfully', data: exercise });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'An error has occurred' });
+  }
+};
+
+module.exports = {
+  getExercises,
+  getExercise,
+  postExcercise,
+  putExercise,
+  deleteExercise,
+};
